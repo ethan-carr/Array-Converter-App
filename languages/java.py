9b9548b9e8a3bd6a46b9a947d2151a6aeef297a2
ethan-carr <111789413+ethan-carr@users.noreply.github.com>
2022-08-24 18:26:50 -0400

    fixed small issue

---
+++ languages/java.py
@@ -0,0 +1,13 @@
+
+class Java:
+    def __init__(self):
+        self.datatype = ""  # not necessary
+        #self.var_name = "" will be handled by the decoder
+        self.splitter = "=" 
+        self.opener = "["
+        self.closer = "]"
+        self.stringsep = ["'", '"'] # Python can take either, hell for nesting
+        self.line_end = ""    # not necessary
+        self.row_deliminator = ","
+        self.row_ender = ";"
+        
